94abf937e6d4e3b871a9a637b2199d6d798efc81
Noel Rodriguez <trushn45@gmail.com>
2016-11-30 14:49:58 +0100

    Sample with koa2;

--- src/app.js
+++ src/app.js
@@ -2,10 +2,12 @@ var koa = require('koa');
 var views = require('koa-views');
 var bodyParser = require('koa-bodyparser');
 const morgan = require('koa-morgan');
+const token = require('serenity-devstack-propagation');
 import api from './api';
 
 //const accessLogStream = fs.createWriteStream(__dirname + '/access.log',
     //                                         { flags: 'a' })
+
 
 var app = new koa();
 
@@ -16,17 +18,36 @@ async function responseTime(ctx, next) {
   console.log("tiempo: ", ms);
   ctx.set('X-Response-Time', ms);
 }
+/*var query = r.query
+var body = r.body
+var header = r.header
 
-const getToken = async (ctx, next) => {
-  console.log("Entramo en getToken");
+if (header && header.authorization) {
+  var parts = header.authorization.split(' ')
+  if (parts.length === 2 && parts[0] === headerKey) {
+    if (token) {
+      err = true
+    }
+    token = parts[1]
+  }
+}*/
+
+const bearertoken = async (ctx, next) => {
+  var token;
+  const bearer =  'bearer';
   const { authorization } = ctx.request.headers;
-  if (authorization == null) {
-    console.log("401 no autorizado");
-    return ctx.throw("JWT token is mandatory",401);
+  const  headers  = ctx.request.headers;
+  if (headers && authorization) {
+    var parts = authorization.split(' ');
+    if (parts.length === 2 && parts[0] === bearer) {
+      token = parts[1];
+      ctx.set(authorization);
+      await next();
+    }else{
+      return ctx.throw("JWT token is bad formatted",401);
+    }
   }else{
-    ctx.set(authorization);
-    console.log("Hemos metido: ",authorization);
-    await next();
+    return ctx.throw("JWT token is mandatory",401);
   }
 }
 
@@ -36,9 +57,10 @@ async function setHeader(ctx,next){
 }
 
 app
+  .use(token())
   .use(morgan())
   .use(bodyParser())
-  .use(getToken)
+//  .use(bearertoken)
   .use(setHeader)
   .use(api.routes())
   .use(api.allowedMethods())
